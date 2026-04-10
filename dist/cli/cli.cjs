db6796fd7aff197a2cb7778bf74ba2e2257f8808
Rommel Caneos <rec0608m@gmail.com>
2026-04-10 19:59:28 +0800

    fix: Add cli.cjs to build script
    - Create cli.cjs wrapper automatically during build
    - Fixes CLI binary not found after installation

--- dist/cli/cli.cjs
+++ dist/cli/cli.cjs
@@ -1,15 +1,7 @@
 #!/usr/bin/env node
-const { spawn } = require('child_process');
-const path = require('path');
-
-const cliPath = path.join(__dirname, 'index.js');
+const { spawn } = require("child_process");
+const path = require("path");
+const cliPath = path.join(__dirname, "index.js");
 const args = process.argv.slice(2);
-
-const child = spawn('node', [cliPath, ...args], {
-  stdio: 'inherit',
-  shell: false
-});
-
-child.on('close', (code) => {
-  process.exit(code);
-});
+const child = spawn("node", [cliPath, ...args], { stdio: "inherit", shell: false });
+child.on("close", (code) => process.exit(code));
